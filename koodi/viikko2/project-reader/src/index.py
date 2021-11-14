34bd0241feb12e1f449a6fc1bf63d89f90a2d8a1
Jusa Mankki <jusamankki@gmail.com>
2021-11-14 11:52:22 +0200

    minor commit

--- koodi/viikko2/project-reader/src/index.py
+++ koodi/viikko2/project-reader/src/index.py
@@ -4,7 +4,7 @@ from project_reader import ProjectReader
 def main():
     url = "https://raw.githubusercontent.com/ohjelmistotuotanto-hy-avoin/python-kevat-2021/main/koodi/viikko3/web-login-robot/pyproject.toml"
     reader = ProjectReader(url)
-    print(reader.get_project())
+    print("PROJCT: \n", reader.get_project())
 
 
 if __name__ == "__main__":
--- koodi/viikko2/project-reader/src/project_reader.py
+++ koodi/viikko2/project-reader/src/project_reader.py
@@ -1,5 +1,6 @@
 from urllib import request
 from project import Project
+import toml
 
 
 class ProjectReader:
@@ -9,7 +10,11 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
-
+        dict = toml.loads(content)
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(
+            dict["tool"]["poetry"]["name"],
+            dict["tool"]["poetry"]["description"],
+            dict["tool"]["poetry"]["dependencies"],
+            dict["tool"]["poetry"]["dev-dependencies"]
+        )
