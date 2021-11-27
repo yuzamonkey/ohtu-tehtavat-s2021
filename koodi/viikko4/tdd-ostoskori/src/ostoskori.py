76dbaea0b587ae12a676a38bda8d24d27b774ef9
Jusa Mankki <jusamankki@gmail.com>
2021-11-27 14:10:00 +0200

    Yhden tuotteen lisäämisen jälkeen ostoskori sisältää ostoksen, jolla sama nimi kuin tuotteella ja lukumäärä 1

--- koodi/viikko4/tdd-ostoskori/src/ostoskori.py
+++ koodi/viikko4/tdd-ostoskori/src/ostoskori.py
@@ -10,18 +10,25 @@ class Ostoskori:
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
-        return len(self.kori)
+        tavaroita = 0
+        for ostos in self.kori:
+            tavaroita += ostos.lukumaara()
+        return tavaroita
 
     def hinta(self):
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
         hinta = 0
-        for tuote in self.kori:
-            hinta += tuote.hinta()
+        for ostos in self.kori:
+            hinta += ostos.hinta()
         return hinta
 
     def lisaa_tuote(self, lisattava: Tuote):
         # lisää tuotteen
-        self.kori.append(lisattava)
+        tuote_korissa = next((ostos for ostos in self.kori if ostos.tuotteen_nimi() == lisattava.nimi()), None)
+        if tuote_korissa is None:
+            self.kori.append(Ostos(lisattava))
+        else:
+            tuote_korissa.muuta_lukumaaraa(1)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -34,8 +41,5 @@ class Ostoskori:
     def ostokset(self):
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
-        ostokset = []
-        for tuote in self.kori:
-            ostokset.append((tuote, 1))
-        return ostokset
+        return self.kori
 
--- koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -40,14 +40,21 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
 
-    def test_kahden_saman_tuotteen_lisäämisen_jälkeen_ostoskorin_hinta_on_sama_kuin_2_kertaa_tuotteen_hinta(self):
+    def test_kahden_saman_tuotteen_lisamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_2_kertaa_tuotteen_hinta(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 6)
 
-    def test_yhden_tuotteen_lisäämisen_jälkeen_ostoskori_sisältää_yhden_ostoksen(self):
+    def test_yhden_tuotteen_lisamisen_jalkeen_ostoskori_sisaltaa_yhden_ostoksen(self):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
+
+    def test_yhden_tuotteen_lisamisen_jalkeen_ostoskori_sisaltaa_ostoksen_jolla_sama_nimi_kuin_tuotteella_ja_lukumara_1(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+        self.assertEqual(ostos.lukumaara(), 1)
