523fbb7defd833146c92d61b9cd13f2b1751028f
Jusa Mankki <jusamankki@gmail.com>
2021-11-27 13:51:05 +0200

    Kahden saman tuotteen lisäämisen jälkeen ostoskorin hinta on sama kuin 2 kertaa tuotteen hinta

--- koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ koodi/viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -39,3 +39,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_saman_tuotteen_lisäämisen_jälkeen_ostoskorin_hinta_on_sama_kuin_2_kertaa_tuotteen_hinta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.hinta(), 6)
