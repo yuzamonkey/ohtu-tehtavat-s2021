4253a485f7832c9f27629edb7495e5c43284688e
Jusa Mankki <jusamankki@gmail.com>
2021-12-06 15:17:01 +0200

    w6e3

--- koodi/viikko6/query-language/src/index.py
+++ koodi/viikko6/query-language/src/index.py
@@ -1,6 +1,6 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, Or, All
 
 
 def main():
@@ -8,23 +8,17 @@ def main():
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    # matcher = And(
-    #     HasAtLeast(5, "goals"),
-    #     HasAtLeast(5, "assists"),
-    #     PlaysIn("PHI")
-    # )
-
     matcher = And(
-        Not(HasAtLeast(1, "goals")),
-        PlaysIn("NYR")
+        HasAtLeast(40, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("NYI"),
+            PlaysIn("BOS")
+        )
     )
-    for player in stats.matches(matcher):
-        print(player)
 
-    matcher = And(
-        HasFewerThan(1, "goals"),
-        PlaysIn("NYR")
-    )
+    #matcher = All()
+
     for player in stats.matches(matcher):
         print(player)
 
--- koodi/viikko6/query-language/src/matchers.py
+++ koodi/viikko6/query-language/src/matchers.py
@@ -1,13 +1,31 @@
+class All:  # (tosi kaikille pelaajille)
+    def matches(self, player):
+        return True
+
+
 class And:
     def __init__(self, *matchers):
         self._matchers = matchers
-    
+
     def matches(self, player):
         for matcher in self._matchers:
             if not matcher.matches(player):
                 return False
-        
+
         return True
+
+
+class Or:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return True
+
+        return False
+
 
 class PlaysIn:
     def __init__(self, team):
@@ -15,6 +33,7 @@ class PlaysIn:
 
     def matches(self, player):
         return player.team == self._team
+
 
 class HasAtLeast:
     def __init__(self, value, attr):
@@ -26,22 +45,16 @@ class HasAtLeast:
 
         return player_value >= self._value
 
-class All: # (tosi kaikille pelaajille)
-    def __init__(self):
-        pass
 
-    def matches(self):
-        pass
-
-
-class Not: # (parameetrina olevan ehdon negaatio)
+class Not:  # (parameetrina olevan ehdon negaatio)
     def __init__(self, cond):
         self._cond = cond
 
     def matches(self, player):
         return not self._cond.matches(player)
 
-class HasFewerThan: #(HasAtLeast-komennon negaatio eli, esim. on vähemmän kuin 10 maalia)
+
+class HasFewerThan:  # (HasAtLeast-komennon negaatio eli, esim. on vähemmän kuin 10 maalia)
     def __init__(self, value, attr):
         self._value = value
         self._attr = attr
