04a29c89a307352f83427038a6c2ab1ffe0b46e4
Jusa Mankki <jusamankki@gmail.com>
2021-12-06 15:14:11 +0200

    w6e2

--- koodi/viikko6/query-language/src/index.py
+++ koodi/viikko6/query-language/src/index.py
@@ -1,18 +1,30 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan
+
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
-    )
+    # matcher = And(
+    #     HasAtLeast(5, "goals"),
+    #     HasAtLeast(5, "assists"),
+    #     PlaysIn("PHI")
+    # )
 
+    matcher = And(
+        Not(HasAtLeast(1, "goals")),
+        PlaysIn("NYR")
+    )
+    for player in stats.matches(matcher):
+        print(player)
+
+    matcher = And(
+        HasFewerThan(1, "goals"),
+        PlaysIn("NYR")
+    )
     for player in stats.matches(matcher):
         print(player)
 
--- koodi/viikko6/query-language/src/matchers.py
+++ koodi/viikko6/query-language/src/matchers.py
@@ -25,3 +25,28 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class All: # (tosi kaikille pelaajille)
+    def __init__(self):
+        pass
+
+    def matches(self):
+        pass
+
+
+class Not: # (parameetrina olevan ehdon negaatio)
+    def __init__(self, cond):
+        self._cond = cond
+
+    def matches(self, player):
+        return not self._cond.matches(player)
+
+class HasFewerThan: #(HasAtLeast-komennon negaatio eli, esim. on vähemmän kuin 10 maalia)
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
